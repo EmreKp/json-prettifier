16aea6b09bb0dd13d952e7e658affbfb3eecc7f9
Emre Kaplan <emre.kaplan@metglobal.com>
2018-10-16 17:33:06 +0300

    Set textarea's font and indentation

--- src/JsonPrettifier.java
+++ src/JsonPrettifier.java
@@ -5,6 +5,8 @@ import java.awt.*;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonPrettifier {
@@ -44,11 +46,18 @@ public class JsonPrettifier {
 		//set textarea
 		JTextArea textArea = new JTextArea("Bu da deneme tekstim");
 		textArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
 
 		//set button
 		JButton prettifyButton = new JButton("Prettify");
 		prettifyButton.addActionListener(e -> {
 			ObjectMapper mapper = new ObjectMapper();
+
+			//set indenter because of default indenter has 2 spaces
+			DefaultPrettyPrinter printer = new DefaultPrettyPrinter();
+			DefaultPrettyPrinter.Indenter indenter = new DefaultIndenter("    ", DefaultIndenter.SYS_LF);
+			printer.indentObjectsWith(indenter);
+			printer.indentArraysWith(indenter);
 
 			Object pojo = null;
 			try {
@@ -58,16 +67,13 @@ public class JsonPrettifier {
 				ex.printStackTrace();
 			}
 
-			String json = null;
+			String json = "";
 			try {
-				json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(pojo);
+				json = mapper.writer(printer).writeValueAsString(pojo);
 			}
 			catch (JsonProcessingException ex) {
 				ex.printStackTrace();
 			}
-
-			//increase indent
-			json = json.replace("\n  ", "\n    ");
 
 			textArea.setText(json);
 		});
